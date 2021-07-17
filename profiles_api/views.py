9b67b0b5d44e71632cc267666516c6291044e48c
aswin <aswin.manoharan@mapmymarketing.ai>
2021-07-17 15:00:19 +0530

    added hello api-view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, api_format=None):
+        """Return list of API View features"""
+        an_api_view = [
+            'Uses HTTP methods as function (get, post, put, patch, delete)',
+            'Is similar to a traditional django view',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to urls',
+        ]
+        return Response({'message': 'Hello!', 'an_api_view': an_api_view})
